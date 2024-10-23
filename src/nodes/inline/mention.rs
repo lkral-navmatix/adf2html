d1329fc86e5ff28588a0afe6c1898f338a6f034f
abendg <abendg@gmail.com>
2024-10-23 16:43:49 +0300

    added custom deserializer for access_level in mention because jira API sends "accessLevel": ""

--- src/nodes/inline/mention.rs
+++ src/nodes/inline/mention.rs
@@ -1,4 +1,9 @@
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::{self, IntoDeserializer as _, Unexpected}, 
+    Deserialize, 
+    Deserializer, 
+    Serialize
+};
 
 use crate::ToHtml;
 
@@ -13,6 +18,7 @@ pub struct Mention {
 #[serde(rename_all = "camelCase")]
 pub struct Attributes {
     pub id: String,
+    #[serde(default, deserialize_with = "deserialize_access_level")]
     pub access_level: Option<AccessLevel>,
     pub text: Option<String>,
     pub user_type: Option<UserType>,
@@ -39,5 +45,25 @@ impl ToHtml for Mention {
     fn to_html(&self) -> String {
         let mention_string = self.attributes.text.clone().unwrap_or_default();
         format!(r#"<span style = "padding: 4px;">{mention_string}</span>"#)
+    }
+}
+
+fn deserialize_access_level<'de, D>(deserializer: D) -> Result<Option<AccessLevel>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let opt: Option<String> = Option::deserialize(deserializer)?;
+
+    match opt.as_deref() {
+        Some("") => Ok(Some(AccessLevel::None)),
+        Some(s) => AccessLevel::deserialize(s.into_deserializer())
+            .map(Some)
+            .or_else(|_: D::Error| {
+                Err(de::Error::invalid_value(
+                    Unexpected::Str(s),
+                    &"a valid access level string",
+                ))
+            }),
+        None => Ok(None),
     }
 }
